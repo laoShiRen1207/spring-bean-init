2adf0e4a71d980e43540dadd495eb26d2009744f
项德华 <15207034473@163.com>
2023-01-06 15:01:12 +0800

    startup

--- src/main/java/com/laoshiren/app/App.java
+++ src/main/java/com/laoshiren/app/App.java
@@ -1,23 +1,17 @@
 package com.laoshiren.app;
 
-import com.laoshiren.bean.CartoonCatAndMouse;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigurationImportSelector;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @Author laoshiren
  * @Date 14:16 2023/1/5
  */
-@SpringBootApplication(scanBasePackages = "com.laoshiren.bean")
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(App.class,args);
-        CartoonCatAndMouse bean = ctx.getBean(CartoonCatAndMouse.class);
-        bean.play();
+        SpringApplication.run(App.class,args);
     }
 
 }
--- src/main/java/com/laoshiren/bean/Dog.java
+++
@@ -1,8 +0,0 @@
-package com.laoshiren.bean;
-
-/**
- * @Author laoshiren
- * @Date 14:16 2023/1/5
- */
-public class Dog {
-}
