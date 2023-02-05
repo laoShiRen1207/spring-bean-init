0fcf74ce40a7afae84ab96f58d6e5c0b35e3c872
laoshiren <xiangdehua@pharmakeyring.com>
2023-02-05 16:24:57 +0800

    web init

--- src/main/java/com/laoshiren/app/App.java
+++ src/main/java/com/laoshiren/app/App.java
@@ -1,29 +1,19 @@
 package com.laoshiren.app;
 
-import com.laoshiren.app.service.impl.MyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @Author laoshiren
  * @Date 10:17 2023/1/10
  */
-@EnableAspectJAutoProxy
 @SpringBootApplication
 @Slf4j
 public class App {
 
     public static void main(String[] args)  {
-        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
-
-        MyService bean = context.getBean(MyService.class);
-        log.info("class "+ bean.getClass());
-        bean.foo();
-
-        context.close();
+        SpringApplication.run(App.class,args);
 
     }
 }
